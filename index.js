36cd47a1aeab0602500fd46a15aa421996633399
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-10-28 14:45:48 -0700

    updated css

--- index.js
+++ index.js
@@ -18,19 +18,35 @@
 //   6:'Saturday',
 // };
 
-$(document).ready(function() {
 
-  
-  const interval = setInterval(getTime, 1000);
-  function getTime() {
-  //   // const watch = $(`<p>${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}</p>`)
-  //   // $('#clock').append(watch);
-    // const calendar = $(`<p id='small'>${dayWeek[date.getDay()]}, Day ${date.getDate()} of Month ${date.getMonth() + 1} of Year ${date.getFullYear()} </p>`)
-    
-    const date = new Date();
-    // document.getElementById('clock').innerHTML = date //.toLocaleTimeString();
-    $("#clock").html(date);
-    }
-  
-  
+$(document).ready(() => {
+
+  const clockElement = $('#clock');
+
+  const updateClock = () => {
+    const justNow = new Date(Date.now());
+
+    const hours = justNow.getHours() < 10 ? "0" + justNow.getHours() : justNow.getHours();
+    const mins = justNow.getMinutes() < 10 ? "0" + justNow.getMinutes() : justNow.getMinutes();
+    const secs = justNow.getSeconds() < 10 ? "0" + justNow.getSeconds() : justNow.getSeconds();
+
+    clockElement.html(`
+      <p>${justNow.toDateString()}</p>
+      <p>${hours}:${mins}:${secs}</p>
+    `);
+  };
+
+  window.setInterval(updateClock, 1000);
+
+  $('#color-buttons').on('click', 'button', function(event) {
+      $('#clock').css('color', `${$(this).attr('id')}`);
+  });    
+
+  $('#align-buttons').on('click', 'button', function(event) {
+    $('#clock').css('text-align', `${$(this).attr('id')}`);
+  });
+
+  $('#font-buttons').on('click', 'button', function(event) {
+    $('#clock').css('font-family', `${$(this).attr('id')}`);
+  });
 });
